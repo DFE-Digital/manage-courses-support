cc466888f4cf639fff8ce154851ed872f0186b5a
Jake Benilov <benilov@gmail.com>
2018-08-10 18:10:25 +0100

    Show organisations, their users and associated UCAS institutions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/access-requests' => 'access_requests#index'
+  get '/organisations' => 'organisations#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
