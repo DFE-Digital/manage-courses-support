4a2958749f9d82e504e1610c4932a6ed0c6736ac
Jake Benilov <benilov@gmail.com>
2018-08-30 08:55:32 +0100

    Add a spec for the organisations index

---
+++ spec/features/organisations_spec.rb
@@ -0,0 +1,69 @@
+require "rails_helper"
+
+RSpec.feature "Organisations", type: :feature do
+  include_context 'when authenticated'
+
+  scenario "Index lists all organisations with their associated users and UCAS institutions" do
+    FactoryBot.create(:organisation,
+      name: "Stellar Alliance / Stellar SCITT",
+      org_id: '12345',
+      users: [
+        FactoryBot.create(:user,
+          email: 'awatson@stellar.org',
+          first_name: 'Alice',
+          last_name: 'Watson',
+        ),
+        FactoryBot.create(:user,
+          email: 'bsmith@stellar.org',
+          first_name: 'Betty',
+          last_name: 'Smith',
+        ),
+      ],
+      institutions: [
+        FactoryBot.create(:institution,
+          inst_full: 'Stellar Alliance',
+          inst_code: 'S01',
+        ),
+        FactoryBot.create(:institution,
+          inst_full: 'Stellar SCITT',
+          inst_code: 'S02',
+        ),
+      ]
+    )
+
+    FactoryBot.create(:organisation,
+      name: "University of Duncree",
+      org_id: '67890',
+      users: [
+        FactoryBot.create(:user,
+          email: 'jbrady@duncree.ac.uk',
+          first_name: 'James',
+          last_name: 'Brady',
+        )
+      ],
+      institutions: [
+        FactoryBot.create(:institution,
+          inst_full: 'University of Duncree',
+          inst_code: 'D07',
+        )
+      ]
+    )
+
+    visit "/organisations"
+
+    within "#organisation12345" do
+      expect(page).to have_text("Alice Watson <awatson@stellar.org>")
+      expect(page).to have_text("Betty Smith <bsmith@stellar.org>")
+      expect(page).to have_text("Stellar Alliance [S01]")
+      expect(page).to have_text("Stellar SCITT [S02]")
+
+      expect(page).to_not have_text("James Brady <jbrady@duncree.ac.uk>")
+      expect(page).to_not have_text("University of Duncree [D07]")
+    end
+
+    within "#organisation67890" do
+      expect(page).to have_text("James Brady <jbrady@duncree.ac.uk>")
+      expect(page).to have_text("University of Duncree [D07]")
+    end
+  end
+end
