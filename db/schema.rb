3d3665320829352ed0a3592733216ebbe1aedf7e
Jake Benilov <benilov@gmail.com>
2018-09-10 22:01:23 +0100

    Add ucas_course to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910183500) do
+ActiveRecord::Schema.define(version: 20180910210005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,11 @@ ActiveRecord::Schema.define(version: 20180910183500) do
   create_table "nctl_organisation", force: :cascade do |t|
     t.string "org_id"
     t.string "nctl_id"
+  end
+
+  create_table "ucas_course", force: :cascade do |t|
+    t.string "inst_code"
+    t.string "crse_code"
   end
 
   create_table "ucas_institution", force: :cascade do |t|
