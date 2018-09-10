c51b45c3011991586f4faa54fc0ca6727abe960f
Jake Benilov <benilov@gmail.com>
2018-09-10 22:09:20 +0100

    Add course_enrichment to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910210208) do
+ActiveRecord::Schema.define(version: 20180910210753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20180910210208) do
     t.datetime "request_date_utc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "course_enrichment", force: :cascade do |t|
+    t.string "inst_code"
+    t.string "ucas_course_code"
+    t.integer "status"
   end
 
   create_table "institution_enrichment", force: :cascade do |t|
