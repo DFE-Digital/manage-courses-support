c07a55174fe4cf0c41d977044c0ec5805045f9c2
Jake Benilov <benilov@gmail.com>
2018-09-12 19:47:58 +0100

    Orgs engagement report tracks orgs with UCAS courses

--- app/models/organisations_engagement_report.rb
+++ app/models/organisations_engagement_report.rb
@@ -5,11 +5,23 @@ class OrganisationsEngagementReport
           DISTINCT org_id
       FROM
           nctl_organisation
+  ),
+  orgs_with_ucas_courses AS (
+      SELECT
+          oi.org_id,
+          count(DISTINCT c.crse_code) number_of_courses
+      FROM
+          mc_organisation_institution oi
+          LEFT OUTER JOIN ucas_course c ON oi.institution_code = c.inst_code
+      GROUP BY
+          oi.org_id
   )
   SELECT
-      SUM(CASE WHEN oa.org_id IS NOT NULL THEN 1 ELSE 0 end) AS orgs_with_allocations
+      SUM(CASE WHEN oa.org_id IS NOT NULL THEN 1 ELSE 0 end) AS orgs_with_allocations,
+      SUM(CASE WHEN ouc.number_of_courses > 0 THEN 1 ELSE 0 end) AS orgs_with_ucas_courses
   FROM
-      orgs_with_allocations oa".freeze
+      orgs_with_allocations oa
+      FULL OUTER JOIN orgs_with_ucas_courses ouc ON oa.org_id = ouc.org_id".freeze
 
   def run
     @results = ActiveRecord::Base.connection.execute(QUERY)[0]
--- spec/models/organisations_engagement_report_spec.rb
+++ spec/models/organisations_engagement_report_spec.rb
@@ -4,10 +4,21 @@ describe OrganisationsEngagementReport, type: :model do
   let(:report) { OrganisationsEngagementReport.new.tap(&:run) }
 
   before do
-    FactoryBot.create_list(:organisation, 5, nctl_organisations_count: 1)
+    @orgs = FactoryBot.create_list(:organisation, 5, nctl_organisations_count: 1)
   end
 
   it "tracks the number of organisations with allocations" do
     expect(report[:orgs_with_allocations]).to eq(5)
+  end
+
+  it "tracks the number of organisations with UCAS courses" do
+    institutions = FactoryBot.create_list(:institution, 4, course_count: 2)
+
+    # attach institutions to orgs
+    @orgs[0..3].zip(institutions) do |org, institution|
+      org.institutions << institution
+    end
+
+    expect(report[:orgs_with_ucas_courses]).to eq(4)
   end
 end
