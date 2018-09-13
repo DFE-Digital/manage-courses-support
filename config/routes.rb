27b71406247b83b0cea468f57339f82ffa220159
Jake Benilov <benilov@gmail.com>
2018-09-13 12:29:26 +0100

    Point the service name to the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/access-requests' => 'access_requests#index'
   get '/organisations' => 'organisations#index'
-  get '/' => 'reports#show_organisations_engagement_report'
+  get '/' => 'reports#show_organisations_engagement_report', as: :organisations_engagement_report
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
