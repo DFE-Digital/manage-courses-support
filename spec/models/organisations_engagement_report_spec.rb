619cb0584b79da48c2b5e7ed180c109587111763
Jake Benilov <benilov@gmail.com>
2018-09-13 12:50:07 +0100

    Update spec from linting feedback

--- spec/models/organisations_engagement_report_spec.rb
+++ spec/models/organisations_engagement_report_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 describe OrganisationsEngagementReport, type: :model do
+  let!(:orgs) { FactoryBot.create_list(:organisation, 5, nctl_organisations_count: 1) }
   let(:report) { OrganisationsEngagementReport.new.tap(&:run) }
-
-  before do
-    @orgs = FactoryBot.create_list(:organisation, 5, nctl_organisations_count: 1)
-  end
 
   it "tracks the number of organisations with allocations" do
     expect(report[:orgs_with_allocations]).to eq(5)
@@ -15,7 +12,7 @@ describe OrganisationsEngagementReport, type: :model do
     institutions = FactoryBot.create_list(:institution, 4, course_count: 2)
 
     # attach institutions to orgs
-    @orgs[0..3].zip(institutions) do |org, institution|
+    orgs[0..3].zip(institutions) do |org, institution|
       org.institutions << institution
     end
 
@@ -27,7 +24,7 @@ describe OrganisationsEngagementReport, type: :model do
     god_user = FactoryBot.create(:user, :god_user, :active)
 
     # attach institutions to orgs
-    @orgs[0..2].zip(users) do |org, user|
+    orgs[0..2].zip(users) do |org, user|
       org.users << user
       org.users << god_user
     end
