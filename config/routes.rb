20e42f97cac99951b270f0507660ac3c6fd1a617
Theodor Vararu <theo@vararu.org>
2018-09-21 14:28:47 +0100

    Add #create, #preview, and #submit actions AccessRequestsController
    This combined with the views offers a complete journey to create,
    preview, and submit manual access requests.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/organisations/without-active-users', to: 'organisations#index_without_active_users'
   get '/organisations-engagement-report', to: 'reports#show_organisations_engagement_report', as: :organisations_engagement_report
   get '/access-requests/:id/approve', to: 'access_requests#approve!', as: :approve_access_request
+  get '/access-requests/create', to: 'access_requests#create', as: :create_access_request
+  get '/access-requests/preview', to: 'access_requests#preview', as: :preview_access_request
+  post '/access-requests/submit', to: 'access_requests#submit', as: :submit_access_request
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/access_requests_spec.rb
+++ spec/features/access_requests_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-BASE_API_URL = "https://www.example.com/api/admin/access-request?accessRequestId=".freeze
+BASE_API_URL = "https://www.example.com".freeze
 
 RSpec.describe "Access requests index", type: :feature do
   include_context 'when authenticated'
@@ -24,7 +24,7 @@ RSpec.describe "Access requests index", type: :feature do
 
   describe "displays notifications when approving access requests" do
     it "for a successful request" do
-      stub_request(:post, "#{BASE_API_URL}#{unapproved_request.id}").to_return(status: 200)
+      stub_request(:post, "#{BASE_API_URL}/api/admin/access-request?accessRequestId=#{unapproved_request.id}").to_return(status: 200)
 
       visit "/access-requests"
       click_link "Approve"
@@ -33,7 +33,7 @@ RSpec.describe "Access requests index", type: :feature do
     end
 
     it "for an unauthorized request" do
-      stub_request(:post, "#{BASE_API_URL}#{unapproved_request.id}").to_return(status: 401)
+      stub_request(:post, "#{BASE_API_URL}/api/admin/access-request?accessRequestId=#{unapproved_request.id}").to_return(status: 401)
 
       visit "/access-requests"
       click_link "Approve"
@@ -42,7 +42,7 @@ RSpec.describe "Access requests index", type: :feature do
     end
 
     it "for a not-found request" do
-      stub_request(:post, "#{BASE_API_URL}#{unapproved_request.id}").to_return(status: 404)
+      stub_request(:post, "#{BASE_API_URL}/api/admin/access-request?accessRequestId=#{unapproved_request.id}").to_return(status: 404)
 
       visit "/access-requests"
       click_link "Approve"
@@ -51,12 +51,54 @@ RSpec.describe "Access requests index", type: :feature do
     end
 
     it "for any other kind of request" do
-      stub_request(:post, "#{BASE_API_URL}#{unapproved_request.id}").to_return(status: 999)
+      stub_request(:post, "#{BASE_API_URL}/api/admin/access-request?accessRequestId=#{unapproved_request.id}").to_return(status: 999)
 
       visit "/access-requests"
       click_link "Approve"
 
       expect(page).to have_text("unknown-error")
+    end
+  end
+
+  describe 'allows submitting manual access requests' do
+    it 'displays link to create manual access requests' do
+      LINK_TEXT = 'Create and approve an access request manually'.freeze
+      visit '/access-requests'
+
+      expect(page).to have_text(LINK_TEXT)
+      click_link LINK_TEXT
+
+      expect(page).to have_text('Create access request')
+    end
+
+    it 'allows submitting access requests' do
+      stub_request(:post, "#{BASE_API_URL}/api/admin/manual-access-request")
+        .with(query: {
+          requesterEmail: 'requester@email.com',
+          targetEmail: 'target@email.com',
+          firstName: 'first',
+          lastName: 'last'
+        })
+        .to_return(status: 200)
+
+      visit '/access-requests/create'
+
+      fill_in 'requester_email', with: 'requester@email.com'
+      fill_in 'target_email', with: 'target@email.com'
+      fill_in 'first_name', with: 'first'
+      fill_in 'last_name', with: 'last'
+
+      click_button 'Preview'
+
+      expect(page).to have_text('Preview access request')
+      expect(page).to have_text('requester@email.com')
+      expect(page).to have_text('target@email.com')
+      expect(page).to have_text('first')
+      expect(page).to have_text('last')
+
+      click_button 'Approve'
+
+      expect(page).to have_text('Successfully approved request')
     end
   end
 end
