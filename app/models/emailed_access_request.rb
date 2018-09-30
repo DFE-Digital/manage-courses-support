b882853e939ec4bb53ed96ef81fe6d3f37cefd18
Jake Benilov <benilov@gmail.com>
2018-09-30 22:17:19 +0100

    Validate that requester exists for emailed access requests
    This will prevent typos when a support agent submits emailed access requests.

--- app/models/emailed_access_request.rb
+++ app/models/emailed_access_request.rb
@@ -3,6 +3,9 @@ class EmailedAccessRequest
 
   attr_reader :requester_email, :target_email, :first_name, :last_name
 
+  validate :requester_exists,
+    unless: Proc.new { |r| r.requester_email.blank? }
+
   def requester_email=(new_value)
     @requester_email = new_value.strip
   end
@@ -19,6 +22,10 @@ class EmailedAccessRequest
     @last_name = new_value.strip
   end
 
+  def requester
+    User.find_by(email: requester_email)
+  end
+
   def manually_approve!
     MANAGE_COURSES_API.manually_approve_access_request(
       requester_email: requester_email,
@@ -26,5 +33,13 @@ class EmailedAccessRequest
       first_name: first_name,
       last_name: last_name,
     )
+  end
+
+private
+
+  def requester_exists
+    unless requester.present?
+      errors.add(:requester_email, "Enter the email of somebody already in the system")
+    end
   end
 end
--- spec/models/emailed_access_request_spec.rb
+++ spec/models/emailed_access_request_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 describe EmailedAccessRequest, type: :model do
+  let(:emailed_request) {
+    EmailedAccessRequest.new(
+      requester_email: 'foo@bar.com',
+      target_email: 'baz@qux.com',
+      first_name: 'baz',
+      last_name: 'qux',
+    )
+  }
+
+  it "validates that the requester exists" do
+    request = EmailedAccessRequest.new(
+      requester_email: 'nonexistent@email.com',
+      target_email: 'baz@qux.com',
+      first_name: 'baz',
+      last_name: 'qux',
+    )
+
+    expect(request).not_to be_valid
+    expect(request.errors[:requester_email]).to eq([
+      "Enter the email of somebody already in the system"
+    ])
+  end
+
   it "can be manually approved" do
+    FactoryBot.create(:user, email: 'foo@bar.com')
     request = stub_request(:post, "https://www.example.com/api/admin/manual-access-request")
       .with(query: {
         requesterEmail: 'foo@bar.com',
@@ -11,12 +35,7 @@ describe EmailedAccessRequest, type: :model do
       })
       .to_return(status: 200)
 
-    EmailedAccessRequest.new(
-      requester_email: 'foo@bar.com',
-      target_email: 'baz@qux.com',
-      first_name: 'baz',
-      last_name: 'qux',
-    ).manually_approve!
+    emailed_request.manually_approve!
 
     expect(request).to have_been_made
   end
