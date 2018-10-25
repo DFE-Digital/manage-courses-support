1c7edae2c845dace79cc43b6dfeb2d6171337c98
Jake Benilov <benilov@gmail.com>
2018-10-25 15:01:28 +0100

    Remove unused association as it's also wrong

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -3,7 +3,4 @@ class Institution < ApplicationRecord
 
   has_and_belongs_to_many :organisations,
     join_table: :organisation_institution
-
-  has_many :courses,
-    foreign_key: :inst_code
 end
