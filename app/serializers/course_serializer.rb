c0e35a1a4ec527694954d438192bf9583c4e83d6
Jake Benilov <benilov@gmail.com>
2018-11-30 12:21:45 +0000

    Add institution code to course API

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,3 +1,7 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :course_code
+  attributes :course_code, :institution_code
+
+  def institution_code
+    object.provider.provider_code
+  end
 end
