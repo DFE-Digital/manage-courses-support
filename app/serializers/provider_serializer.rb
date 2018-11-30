6472ff0089901573eab3745374dace5552c017ee
Jake Benilov <benilov@gmail.com>
2018-11-30 16:59:05 +0000

    Add placeholder fields on provider API

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,5 +1,6 @@
 class ProviderSerializer < ActiveModel::Serializer
   attributes :institution_code, :institution_name
+  attributes :institution_type, :accrediting_provider
 
   def institution_code
     object.provider_code
@@ -7,5 +8,13 @@ class ProviderSerializer < ActiveModel::Serializer
 
   def institution_name
     object.provider_name
+  end
+
+  def institution_type
+    'Y' # TODO: wire up data
+  end
+
+  def accrediting_provider
+    # TODO: pull this thru from UCAS import
   end
 end
