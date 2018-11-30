e3a55b0629a7f7e0f93880ad4f17f3a9518013d5
Jake Benilov <benilov@gmail.com>
2018-11-30 15:37:50 +0000

    Add maths/english/science placeholders to course API

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,6 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :course_code, :name, :modular, :study_mode
+  attributes :course_code, :name, :study_mode
+  attributes :modular, :english, :maths, :science
   has_many :site_statuses, key: :campuses
   has_one :provider
   has_one :accrediting_provider
@@ -7,5 +8,17 @@ class CourseSerializer < ActiveModel::Serializer
   def modular
     # TODO: pull in from UCAS, possible values "M" or nil
     nil
+  end
+
+  def english
+    # TODO: pull in from UCAS, possible values 1, 2, 3 or 9
+  end
+
+  def maths
+    # TODO: pull in from UCAS, possible values 1, 2, 3 or 9
+  end
+
+  def science
+    # TODO: pull in from UCAS, possible values 1, 2, 3 or 9
   end
 end
