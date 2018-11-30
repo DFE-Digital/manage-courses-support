590ceef2bce9ed779420e0b2786402779b35b9de
Jake Benilov <benilov@gmail.com>
2018-11-30 14:54:41 +0000

    Introduce SiteStatus to join courses and sites

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,6 @@ class Course < ApplicationRecord
   self.table_name = "course"
 
   belongs_to :provider
-  has_and_belongs_to_many :sites
+  has_many :site_statuses
+  has_many :sites, through: :site_statuses
 end
