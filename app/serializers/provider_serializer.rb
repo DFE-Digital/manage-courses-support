d83b2e8df3c563a78a0e05b068f1303bff321df7
Jake Benilov <benilov@gmail.com>
2018-11-30 16:36:11 +0000

    Add institution name to provider API

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,7 +1,11 @@
 class ProviderSerializer < ActiveModel::Serializer
-  attributes :institution_code
+  attributes :institution_code, :institution_name
 
   def institution_code
     object.provider_code
+  end
+
+  def institution_name
+    object.provider_name
   end
 end
