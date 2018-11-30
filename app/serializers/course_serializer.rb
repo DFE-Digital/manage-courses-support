ae9d489805692026f5db409ea8a56b6a840da5dd
Jake Benilov <benilov@gmail.com>
2018-11-30 12:24:18 +0000

    Add placeholder modular flag to course API

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,7 +1,12 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :course_code, :institution_code
+  attributes :course_code, :institution_code, :modular
 
   def institution_code
     object.provider.provider_code
+  end
+
+  def modular
+    # TODO: pull in from UCAS, possible values "M" or nil
+    nil
   end
 end
