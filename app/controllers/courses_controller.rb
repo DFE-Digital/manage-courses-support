6f2145e81eb52d49dbbf3e2aeb15d8216f594c35
Jake Benilov <benilov@gmail.com>
2018-12-04 15:42:18 +0000

    Increase the course API page size to 100

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ActionController::API
   def index
     @courses = Course.all.includes(:sites, :provider, :site_statuses)
-    paginate json: @courses
+    paginate json: @courses, per_page: 100
   end
 end
