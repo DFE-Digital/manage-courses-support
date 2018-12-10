ba60c92efa19c87f3838044801b629c3450c0940
Jake Benilov <benilov@gmail.com>
2018-12-10 17:46:51 +0000

    Add more TODOs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/providers', to: 'providers#index'
   get '/subjects', to: 'subjects#index'
   get '/organisations', to: 'organisations#index'
+
+  # TODO: version the API
+  # TODO: add year parameter
   get '/organisations/without-active-users', to: 'organisations#index_without_active_users'
   get '/organisations-engagement-report', to: 'reports#show_organisations_engagement_report', as: :organisations_engagement_report
 
