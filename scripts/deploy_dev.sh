915a72356fcf98b2a9e761e665231b1b72717e31
Tim Abell <tim@timwise.co.uk>
2019-01-24 10:48:26 +0000

    DRY the deploy scripts

--- scripts/deploy_dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-git push --force https://\$bat-dev-manage-courses-support-app:$APP_CREDENTIALS_DEV_PWD@bat-dev-manage-courses-support-app.scm.azurewebsites.net:443/bat-dev-manage-courses-support-app.git HEAD:refs/heads/master
--- scripts/deploy_staging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-git push --force https://\$bat-staging-manage-courses-support-app:$APP_CREDENTIALS_STAGING_PWD@bat-staging-manage-courses-support-app.scm.azurewebsites.net:443/bat-staging-manage-courses-support-app.git HEAD:refs/heads/master
