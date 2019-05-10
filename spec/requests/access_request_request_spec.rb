cab8032bf535762ab8a2d81d1e669f436ec1b068
Tim Abell <tim@timwise.co.uk>
2019-05-10 16:58:40 +0100

    [1363] Better rspec naming
    PR feedback

--- spec/requests/access_request_request_spec.rb
+++ spec/requests/access_request_request_spec.rb
@@ -4,7 +4,7 @@ describe "Access Requests", type: :request do
   describe 'POST #approve' do
     let(:credentials) { ActionController::HttpAuthentication::Basic.encode_credentials("super.admin@education.gov.uk", "beta") }
     let(:auth_header) { { 'HTTP_AUTHORIZATION' => credentials } }
-    it 'succeeds' do
+    it 'posts to backend approve endpoint' do
       approve_mock = stub_api_v2_request "/access_requests/1/approve", nil, :post
       post '/access-requests/1/approve', headers: auth_header
       expect(response).to redirect_to action: :inform_publisher
