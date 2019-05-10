0a8de57ea89e40e117b9619fe7513f9c70c2e93a
Tim Abell <tim@timwise.co.uk>
2019-05-10 15:21:10 +0100

    [1363] Check approve backend api call is made in spec
    Pairing Tim+David

--- spec/requests/access_request_request_spec.rb
+++ spec/requests/access_request_request_spec.rb
@@ -5,9 +5,10 @@ describe "Access Requests", type: :request do
     let(:credentials) { ActionController::HttpAuthentication::Basic.encode_credentials("super.admin@education.gov.uk", "beta") }
     let(:auth_header) { { 'HTTP_AUTHORIZATION' => credentials } }
     it 'succeeds' do
-      stub_api_v2_request "/access_requests/1/approve", nil, :post
+      approve_mock = stub_api_v2_request "/access_requests/1/approve", nil, :post
       post '/access-requests/1/approve', headers: auth_header
       expect(response).to redirect_to action: :inform_publisher
+      assert_requested approve_mock
     end
   end
 end
