86282bbd04972897d62abf9950f5875ee4d3eebe
Tim Abell <tim@timwise.co.uk>
2019-05-13 09:55:50 +0100

    [1363] Refactor approve model spec
    PR feedback & pairing with Misha
    
    * Pull out common elements for re-use across extra "it" blocks if
      needed.
    * Don't change global thread state to avoid possible conflicts with
      future tests, mock the fetch method instead.
    * Specify token in mock to ensure headers are validated.

--- spec/models/access_request_api_spec.rb
+++ spec/models/access_request_api_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 describe AccessRequestAPI, type: :model do
-  it "posts to backend approve endpoint" do
-    approve_mock = stub_api_v2_request "/access_requests/1/approve", nil, :post
-    Thread.current[:manage_courses_backend_token] = "one-ring-to-rule-them-all"
-    access_request = AccessRequestAPI.new(id: 1)
-    access_request.approve
-    assert_requested approve_mock
+  describe "#approve" do
+    let!(:approve_mock) { stub_api_v2_request "/access_requests/1/approve", nil, :post, token: "one-ring-to-rule-them-all" }
+
+    before do
+      allow(Thread.current).to receive(:fetch)
+                                 .with(:manage_courses_backend_token)
+                                 .and_return("one-ring-to-rule-them-all")
+    end
+
+    it "posts to backend approve endpoint" do
+      access_request = AccessRequestAPI.new(id: 1)
+      access_request.approve
+      assert_requested approve_mock
+    end
   end
 end
