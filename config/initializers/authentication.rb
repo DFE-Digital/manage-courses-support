b513556d89ee1f4bf69cfcec7d9ed7d11d92f355
Tim Abell <tim@timwise.co.uk>
2019-05-13 15:51:47 +0100

    [1363] Allow qa environment when loading config
    Existing code raised error when env was qa

--- config/initializers/authentication.rb
+++ config/initializers/authentication.rb
@@ -1,7 +1,7 @@
 if Rails.env.development? || Rails.env.test?
   AUTHENTICATION = { 'super.admin@education.gov.uk' => 'beta' }.freeze # email matches seeds.rb
-elsif Rails.env.production? && ENV['AUTHENTICATION_CREDENTIALS'].present?
-  AUTHENTICATION = JSON.parse(ENV['AUTHENTICATION_CREDENTIALS'])
+elsif ENV['AUTHENTICATION_CREDENTIALS'].blank?
+  raise "In non-development mode, AUTHENTICATION_CREDENTIALS needs to be set with JSON key-value pairs with email and password"
 else
-  raise "In production mode, AUTHENTICATION_CREDENTIALS needs to be set with JSON key-value pairs with email and password"
+  AUTHENTICATION = JSON.parse(ENV['AUTHENTICATION_CREDENTIALS'])
 end
