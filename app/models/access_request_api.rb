8e1e84173efe5b3c43c1d924ed18c39c1601218e
David Gisbey <davidgisbey@gmail.com>
2019-05-16 15:52:18 +0100

    [1367] Pull in access requests from the backend API

--- app/models/access_request_api.rb
+++ app/models/access_request_api.rb
@@ -3,5 +3,9 @@ class AccessRequestAPI < Base
     "access_requests"
   end
 
+  def recipient
+    User.new(first_name: first_name, last_name: last_name, email: requester_email)
+  end
+
   custom_endpoint :approve, on: :member, request_method: :post
 end
--- spec/requests/access_request_request_spec.rb
+++ spec/requests/access_request_request_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe "Access Requests", type: :request do
+  let(:credentials) { ActionController::HttpAuthentication::Basic.encode_credentials("super.admin@education.gov.uk", "beta") }
+  let(:auth_header) { { 'HTTP_AUTHORIZATION' => credentials } }
+
+
+  describe 'GET #index' do
+    it 'gets to backend index endpoint' do
+      index_mock = stub_api_v2_request "/access_requests?include=requester", nil, :get
+      get '/access-requests', headers: auth_header
+      assert_requested index_mock
+    end
+  end
+
   describe 'POST #approve' do
-    let(:credentials) { ActionController::HttpAuthentication::Basic.encode_credentials("super.admin@education.gov.uk", "beta") }
-    let(:auth_header) { { 'HTTP_AUTHORIZATION' => credentials } }
     it 'posts to backend approve endpoint' do
       approve_mock = stub_api_v2_request "/access_requests/1/approve", nil, :post
       post '/access-requests/1/approve', headers: auth_header
