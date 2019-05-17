4b01c2e18a6c7be9cf5c4fd27d8c25e9b26b5d9e
Tim Abell <tim@timwise.co.uk>
2019-05-17 11:32:32 +0100

    [1491] Address PR feedback

--- spec/models/access_request_api_spec.rb
+++ spec/models/access_request_api_spec.rb
@@ -1,16 +1,8 @@
 require 'rails_helper'
 
 describe AccessRequestAPI, type: :model do
-  let(:token) { "one-ring-to-rule-them-all" }
-
-  before do
-    allow(Thread.current).to receive(:fetch)
-                                 .with(:manage_courses_backend_token)
-                                 .and_return(token)
-  end
-
   describe "#approve" do
-    let!(:approve_mock) { stub_api_v2_request "/access_requests/1/approve", nil, :post, token: token }
+    let!(:approve_mock) { stub_api_v2_request "/access_requests/1/approve", nil, :post }
 
     it "posts to backend approve endpoint" do
       access_request = AccessRequestAPI.new(id: 1)
@@ -20,7 +12,7 @@ describe AccessRequestAPI, type: :model do
   end
 
   describe "#create" do
-    let!(:create_mock) { stub_api_v2_request "/access_requests", nil, :post, token: token }
+    let!(:create_mock) { stub_api_v2_request "/access_requests", nil, :post }
 
     it "posts to backend create endpoint" do
       AccessRequestAPI.create
