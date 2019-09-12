ab9a873fb144d667e6a475256e9e7ca3e377136f
markmead <itsmarkmead@gmail.com>
2019-09-12 11:41:01 +0100

    add `govuk-link` class to links

--- app/helpers/organisation_helper.rb
+++ app/helpers/organisation_helper.rb
@@ -2,7 +2,7 @@ module OrganisationHelper
   def user_details(user, dfe_signin_deeplink: false)
     if dfe_signin_deeplink && user.sign_in_user_id.present?
       link_to "#{user.first_name} #{user.last_name} <#{user.email}>",
-              dfe_signin_user_audit_link(user)
+              dfe_signin_user_audit_link(user), class: 'govuk-link'
     else
       "#{user.first_name} #{user.last_name} <#{user.email}>"
     end
@@ -10,7 +10,7 @@ module OrganisationHelper
 
   def provider_details(provider)
     link_to "#{provider.provider_name} [#{provider.provider_code}]",
-            provider_url_on_publish_teacher_training_courses(provider)
+            provider_url_on_publish_teacher_training_courses(provider), class: 'govuk-link'
   end
 
   def provider_url_on_publish_teacher_training_courses(provider)
